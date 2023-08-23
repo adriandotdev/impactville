30d039b064869f17ead213f03c136e809d5585d7
Adrian Nads <marcelonads@gmail.com>
2023-08-24 06:09:39 +0800

    added about page

--- landing-page/index.js
+++ landing-page/index.js
@@ -1,5 +1,10 @@
 const productItems = document.querySelectorAll('.product-item');
 
+// About Functionalities
+const aboutDescription = document.getElementById('about-text-description');
+const aboutBtnShowMore = document.getElementById('btn-show-more')
+const truncateIcon = document.getElementById('truncate');
+
 function animateProductItems() {
 
     productItems.forEach(item => {
@@ -14,6 +19,26 @@ function animateProductItems() {
         }
     });
 }
+
+let isAboutDescriptionExpanded = false;
+
+aboutBtnShowMore.addEventListener('click', () => {
+
+    if (isAboutDescriptionExpanded) {
+        aboutBtnShowMore.textContent = 'Read More';
+        aboutDescription.style.maxHeight = '70px';
+        aboutDescription.style.overflow = 'hidden';
+        isAboutDescriptionExpanded = false;
+        truncateIcon.style.display = 'block';
+    }
+    else {
+        aboutBtnShowMore.textContent = 'Show Less';
+        aboutDescription.style.maxHeight = 'max-content';
+        aboutDescription.style.overflow = 'auto';
+        isAboutDescriptionExpanded = true;
+        truncateIcon.style.display = 'none';
+    }
+});
 
 // Scroll event
 let isScrolling = false;
